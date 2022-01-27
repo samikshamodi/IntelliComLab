b3baf2f433f56e17c8f14a8045b190aa6c3a76f3
Vaibhav Girish <vaibhav19121@iiitd.ac.in>
2022-01-27 22:26:45 +0530

    Added info

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    hamburger.classList.add("sticky");
+  } else {
+    hamburger.classList.remove("sticky");
+  }
+}
