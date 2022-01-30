b9555dd72dfb9a17fecc947128ccbaa57fa0267f
Samiksha Modi <samikshamodi7@gmail.com>
2022-01-31 03:00:53 +0530

    navbar underline

--- script.js
+++ script.js
@@ -2,10 +2,3 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
-
-$(document).ready(function () {
-  $(".navbar-nav > a").click(function (e) {
-   $(".navbar-nav > a").removeClass("active");
-   $(this).addClass("active");
-    });
-});
